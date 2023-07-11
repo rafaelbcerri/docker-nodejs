8fcb6af78b49968eaf9589a3e22d043f5bee1e96
Rafael Cerri <rafaelbcerri@gmail.com>
2023-07-11 15:24:50 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,8 @@
   // Your JavaScript
   console.log('Hello World');
   document.querySelector('.nav-list-link').textContent = 'Bananna';
+  console.log('LS', LS);
 })();
+
+console.log('Hello World');
+console.log('LS', LS);
